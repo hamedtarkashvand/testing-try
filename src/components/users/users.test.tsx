fdddbe1bf46f89ebab7be3b8e5ae79ff80c110a4
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2023-10-29 14:07:03 +0330

    create components

---
+++ src/components/users/users.test.tsx
@@ -0,0 +1,9 @@
+import { render , Screen } from "@testing-library/react";
+import { Users } from "./users";
+
+describe('d', () => {
+    const data = ['css' , 'js' , 'html']
+    test('ds', () => {
+        render(<Users  />)
+    })
+})
