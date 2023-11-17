843c21108cfc7c814ec8f1afd9217cceb2d27edb
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2023-11-17 18:19:53 +0330

    dsf

--- src/components/counter-tow/counterTow.test.tsx
+++ src/components/counter-tow/counterTow.test.tsx
@@ -7,7 +7,7 @@ describe('testing counter tow ', () => {
   test('should component have heading element', () => {
     render(<CounterTwo count={0} />)
     const headingElement = screen.getByRole('heading');
-    expect(headingElement).toHaveTextContent('Counter Two6');
+    expect(headingElement).toHaveTextContent('Counter Two6 ');
   });
 
 })
--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,24 +1,26 @@
+import '@testing-library/jest-dom';
+
 import { screen, render } from '@testing-library/react';
 
 import { Skills } from './skills';
 
 describe('Skills', () => {
-  const skills = ['js','next','css'];
+  const skills = ['js', 'next', 'css'];
 
   test(' corrects', () => {
     render(<Skills skills={skills} />);
-      const ulElement = screen.getByRole('list');
-      expect(ulElement).toBeInTheDocument()
+    const ulElement = screen.getByRole('list');
+    expect(ulElement).toBeInTheDocument();
   });
 
   test('list skills ', () => {
-    render(<Skills skills={skills} />)
+    render(<Skills skills={skills} />);
     const listElement = screen.getAllByRole('listitem');
     expect(listElement).toHaveLength(listElement.length);
   });
 
   test('has button Login ', () => {
-    render(<Skills skills={skills} />)
+    render(<Skills skills={skills} />);
     const buttonElement = screen.getByRole('button', {
       name: 'Login',
     });
@@ -29,17 +31,15 @@ describe('Skills', () => {
     render(<Skills skills={skills} />);
     const buttonElement = screen.queryByRole('button', {
       name: 'Start learning',
-    })
+    });
     expect(buttonElement).not.toBeInTheDocument();
   });
 
-
-  test('async ', async() => {  
-    render(<Skills skills={skills} />)
-    const buttonElement =  await screen.findByRole('button', {
+  test('async ', async () => {
+    render(<Skills skills={skills} />);
+    const buttonElement = await screen.findByRole('button', {
       name: 'Start learning',
     });
     expect(buttonElement).toBeInTheDocument();
-  })
-
+  });
 });
