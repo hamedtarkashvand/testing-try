b734ce43a7517af4d281bffda48d6477a2c93a70
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2023-11-18 18:07:49 +0330

    mock service

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,28 +1,19 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse  } from 'msw';
 
 export const handlers = [
   // Intercept the "GET /resource" request.
   http.get('https://jsonplaceholder.typicode.com/users', () => {
     // And respond with a "text/plain" response
     // with a "Hello world!" text response body.
-    return HttpResponse.json(
+    return  HttpResponse.json(
       [
         {
-          user: 'marayam',
+          name: 'marayam',
         },
         {
-          user: 'hamed',
-        },
-        {
-          user: 'mahmood',
-        },
-        {
-          user: 'saleh',
-        },
-      ],
-      {
-        status: 200,
-      }
+          name: 'hamed',
+        }
+      ]
     );
   }),
 ];
--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,12 +1,32 @@
-import { render , screen } from "@testing-library/react";
-import { Users } from "./users";
+import { render, screen, waitFor } from '@testing-library/react';
+import { Users } from './users';
+import { server } from '../../../mocks/services';
+import { HttpResponse, http } from 'msw';
 
 describe('testing mock services', () => {
+  test('render correctly ', () => {
+    render(<Users />);
+    const headingElement = screen.getByText('Users');
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('render users', async () => {
+    render(<Users />);
+    const users = await screen.findAllByRole('listitem');
+    screen.debug();
+
+    expect(users).toHaveLength(2);
+  });
+
+  test('should show error messages moment service error', async () => {
+    server.use(
+      http.get('https://jsonplaceholder.typicode.com/users', () => {
+        return HttpResponse.json({ error: 'Not Authorized' }, { status: 401 });
+      })
+    );
+    render(<Users />);
+      const errorMessage = await screen.findByText('Error fetching users ');
+      await waitFor(()=> expect(errorMessage).toBeInTheDocument())
    
-    test('sholde render listitems', async () => {
-        render(<Users />)
-        const elemntsUsers = await screen.findAllByRole('listitem');
-        screen.debug()
-        expect(elemntsUsers).toHaveLength(4)
-    })
-})
+  });
+});
